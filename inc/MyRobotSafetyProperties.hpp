b9d3446838ceb73f7ef17b080feab55e7e685563
dany ferreira <dany.ferreira@ost.ch>
2026-04-21 09:51:02 +0000

    feat: add safety levels and in/output

--- inc/MyRobotSafetyProperties.hpp
+++ inc/MyRobotSafetyProperties.hpp
@@ -13,17 +13,41 @@ public:
     // Define all possible events
     eeros::safety::SafetyEvent doSystemOff;
     eeros::safety::SafetyEvent doSystemOn;
+    eeros::safety::SafetyEvent systemOn;
+    eeros::safety::SafetyEvent systemOff;
+    eeros::safety::SafetyEvent doMotorPowerOn;
+    eeros::safety::SafetyEvent doMotorPowerOff;
+    eeros::safety::SafetyEvent emergencyOn;
+    eeros::safety::SafetyEvent emergencyOver;
+    eeros::safety::SafetyEvent startDriving;
+    eeros::safety::SafetyEvent startBraking;
+    eeros::safety::SafetyEvent stopParking;
+    eeros::safety::SafetyEvent startParking;
+
+
+
+
 
     // Defina all possible levels
     eeros::safety::SafetyLevel slSystemOff;
     eeros::safety::SafetyLevel slSystemOn;
+    eeros::safety::SafetyLevel slSwitchingOn;
+    eeros::safety::SafetyLevel slSwitchingOff;
+    eeros::safety::SafetyLevel slMotorPowerOn;
+    eeros::safety::SafetyLevel slEmergency;
+    eeros::safety::SafetyLevel slDriving;
+    eeros::safety::SafetyLevel slBraking;
+    eeros::safety::SafetyLevel slParking;
+
 
 private:
     // Define all critical outputs
-    // eeros::hal::Output<bool>* ...;
+    eeros::hal::Output<bool>* onBoardLEDgreen;
+    eeros::hal::Output<bool>* onBoardLEDred;
 
     // Define all critical inputs
-    // eeros::hal::Input<bool>* ...;
+    eeros::hal::Input<bool>* onBoardButtonPause;
+    eeros::hal::Input<bool>* onBoardButtonMode;
 
     ControlSystem &cs;
 };
--- src/MyRobotSafetyProperties.cpp
+++ src/MyRobotSafetyProperties.cpp
@@ -4,39 +4,99 @@ MyRobotSafetyProperties::MyRobotSafetyProperties(ControlSystem &cs, double dt)
     : cs(cs),
     
       slSystemOff("System is offline"),
+      slSwitchingOff("System is switching off"),
+      slSwitchingOn("System is switching on"),
+      slEmergency("Emergency state"),
       slSystemOn("System is online"),
+      slMotorPowerOn("Motor power is on"),
+      slParking("Parking"),
+      slBraking("Braking"),
+      slDriving("Driving"),
 
       doSystemOn("Startup the system"),
-      doSystemOff("Shutdown the system")
+      doSystemOff("Shutdown the system"),
+      systemOn("System is on"),
+      systemOff("System is off"),
+      doMotorPowerOn("Turn on motor power"),
+      doMotorPowerOff("Turn off motor power"),
+      emergencyOn("Emergency triggered"),
+      emergencyOver("Emergency over"),
+      startDriving("Start driving"),
+      startBraking("Start braking"),
+      stopParking("Stop parking"),
+      startParking("Start parking"),
 {
     eeros::hal::HAL &hal = eeros::hal::HAL::instance();
 
     // Declare and add critical outputs
-    // ... = hal.getLogicOutput("...");
+    greenLED = hal.getLogicOutput("onBoardLEDgreen");
+    redLED = hal.getLogicOutput("onBoardLEDred");
 
-    // criticalOutputs = { ... };
+    criticalOutputs = { greenLED, redLED };
 
     // Declare and add critical inputs
-    // ... = eeros::hal::HAL::instance().getLogicInput("...", ...);
+    buttonPause = eeros::hal::HAL::instance().getLogicInput("onBoardButtonPause");
+    buttonMode = eeros::hal::HAL::instance().getLogicInput("onBoardButtonMode");
 
-    // criticalInputs = { ... };
+    criticalInputs = { buttonPause, buttonMode };
 
     // Add all safety levels to the safety system
     addLevel(slSystemOff);
+    addLevel(slSwitchingOff);
+    addLevel(slSwitchingOn);
+    addLevel(slEmergency);
     addLevel(slSystemOn);
+    addLevel(slMotorPowerOn);
+    addLevel(slParking);
+    addLevel(slBraking);
+    addLevel(slDriving);
+
 
     // Add events to individual safety levels
-    slSystemOff.addEvent(doSystemOn, slSystemOn, kPublicEvent);
-    slSystemOn.addEvent(doSystemOff, slSystemOff, kPublicEvent);
+    slSystemOff.addEvent(doSystemOn, slSwitchingOn, kPublicEvent);
+    // slSystemOn.addEvent(doSystemOff, slSwitchingOff, kPublicEvent);
+    slSwitchingOff.addEvent(systemOff, slSystemOff, kPublicEvent);
+    slSwitchingOn.addEvent(systemOn, slSystemOn, kPublicEvent);
+    // slEmergency.addEvent(doSystemOff, slSwitchingOff, kPublicEvent);
+    slSystemOn.addEvent(doPowerMotorOn, slMotorPowerOn, kPublicEvent);
+    slMotorPowerOn.addEvent(doMotorPowerOff, slSystemOn, kPublicEvent);
+    slSystemOn.addEvent(emergencyOn, slEmergency, kPublicEvent);
+    slMotorPowerOn.addEvent(emergencyOn, slEmergency, kPublicEvent);
+    slParking.addEvent(emergencyOn, slEmergency, kPublicEvent);
+    slMotorPowerOn.addEvent(startDriving, slDriving, kPublicEvent);
+    slDriving.addEvent(startBraking, slBraking, kPublicEvent);
+    slBraking.addEvent(startDriving, slDriving, kPublicEvent);
+    slBraking.addEvent(startParking, slParking, kPublicEvent);
+    slParking.addEvent(stopParking, slMotorPowerOn, kPublicEvent);
 
     // Add events to multiple safety levels
-    // addEventToAllLevelsBetween(lowerLevel, upperLevel, event, targetLevel, kPublicEvent/kPrivateEvent);
+    addEventToAllLevelsBetween(slEmergency, slSystemOn, slMotorPowerOn, slParking, doSystemOff, slSwitchingOff, kPublicEvent);
+    addEventToAllLevelsBetween(slSystemOn, slMotorPowerOn, slParking, emergencyOn, slEmergency, kPublicEvent);
+
 
     // Define input actions for all levels
-    // level.setInputActions({ ... });
+    slSystemOff.setInputActions({           ignore(buttonPause),                    ignore(buttonMode) });
+    slSwitchingOff.setInputActions({        ignore(buttonPause),                    ignore(buttonMode) });
+    slBraking.setInputActions({             ignore(buttonPause),                    ignore(buttonMode) });
+    slSwitchingOn.setInputActions({         ignore(buttonPause),                    ignore(buttonMode) });
+    slEmergency.setInputActions({           ignore(buttonPause),                    check(buttonMode, true, emergencyOver) });
+    slSystemOn.setInputActions({            check(buttonPause, true, emergency),    ignore(buttonMode) });
+    slMotorPowerOn.setInputActions({        check(buttonPause, true, emergency),    ignore(buttonMode) });
+    slParking.setInputActions({             check(buttonPause, true, emergency),    ignore(buttonMode) });
+    slBraking.setInputActions({             ignore(buttonPause),                    ignore(buttonMode) });
+    slDriving.setInputActions({             check(buttonPause, true, startBraking), ignore(buttonMode) });
+
 
     // Define output actions for all levels
-    // level.setOutputActions({ ... });
+    slSystemOff.setOutputActions({           set(greenLED, false),   set(redLED, false) });
+    slSwitchingOff.setOutputActions({        set(greenLED, false),   set(redLED, true) });
+    slSwitchingOn.setOutputActions({         set(greenLED, false),   set(redLED, false) });
+    slEmergency.setOutputActions({           set(greenLED, true),    set(redLED, true) });
+    slSystemOn.setOutputActions({            set(greenLED, true),    set(redLED, false) });
+    slMotorPowerOn.setOutputActions({        set(greenLED, true),    set(redLED, false) });
+    slDriving.setOutputActions({             set(greenLED, true),    set(redLED, false) });
+    slParking.setOutputActions({             set(greenLED, true),    set(redLED, false) });
+    slBraking.setOutputActions({             set(greenLED, true),    set(redLED, false) });
 
     // Define and add level actions
     slSystemOff.setLevelAction([&](SafetyContext *privateContext) {
